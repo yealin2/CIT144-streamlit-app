56c99bdb8a7c3c54866652c0c38e6478d7f0b095
Yerin Son <sonyerin1004@gmail.com>
2026-03-04 15:44:24 -0800

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -86,7 +86,7 @@ st.session_state.messages.append({"role": "user", "content": prompt})
 def response_generator():
     response = ai_ask("Pretend you are a very friendly and helpful person.  Please provide a response given the provided context.  Please provide the response only with no before or after commentary.",
                       data=st.session_state.messages,
-                      api_key=st.secrets["apikey"])
+                      api_key=st.secrets["api_key"])
 
     for word in response.split():
         yield word + " "
